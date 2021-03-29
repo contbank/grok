373718898520cb13735d6e533ac3ec7e138aca21
Diego Oliveira <diego.oliveira@ficatranquilo.com.br>
2021-03-29 13:25:06 -0300

    copying body to request

--- max_body_size_middleware.go
+++ max_body_size_middleware.go
@@ -1,6 +1,8 @@
 package grok
 
 import (
+	"bytes"
+	"io"
 	"io/ioutil"
 	"net/http"
 
@@ -17,13 +19,18 @@ func SetMaxBodyBytesMiddleware(maxMegaBytes int64) gin.HandlerFunc {
 
 		c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, maxMBytes<<20)
 
-		_, err := ioutil.ReadAll(c.Request.Body)
+		bodyCopy := new(bytes.Buffer)
+		_, err := io.Copy(bodyCopy, c.Request.Body)
 
 		if err != nil {
 			logrus.WithField("error", err).Error("error on max body middleware")
 			entityTooLarge := NewError(http.StatusRequestEntityTooLarge, "payload too large")
 			c.AbortWithStatusJSON(http.StatusRequestEntityTooLarge, entityTooLarge)
+
 			return
 		}
+
+		bodyData := bodyCopy.Bytes()
+		c.Request.Body = ioutil.NopCloser(bytes.NewReader(bodyData))
 	}
 }
