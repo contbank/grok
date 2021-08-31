9dd094b45c08fe4f81dd789fbcf5978e82c39f74
Joacir S. Ramos Junior <joacir.ramos@pismo.io>
2021-08-31 15:42:06 -0300

    Ajustes dos DDDs

--- utils.go
+++ utils.go
@@ -215,16 +215,23 @@ func GeneratorCellphone() string {
 	phoneString := ""
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	dddArray := [3]int{11, 21, 51}
-	ddd := dddArray[rand.Intn(2)]
+	var dddArray = []string {
+		"11", "12", "13", "14", "15", "16", "17", "18", "19",
+		"21", "22", "24", "27", "28", "31", "32", "33", "34",
+		"35", "37", "38", "41", "42", "43", "44", "45", "46",
+		"47", "48", "49", "51", "53", "54", "55", "61", "62",
+		"63", "64", "65", "66", "67", "68", "69", "71", "73",
+		"74", "75", "77", "79", "81", "82", "83", "84", "85",
+		"86", "87", "88", "89", "91", "92", "93", "94", "95",
+		"96", "97", "98", "99",
+	}
 
 	phone := rand.Perm(8)
-
 	for _, c := range phone {
 		phoneString += strconv.Itoa(c)
 	}
 
-	phoneString = strconv.Itoa(ddd) + "9" + phoneString
+	phoneString = dddArray[rand.Intn(2)] + "9" + phoneString
 
 	return phoneString
 }
