9bb8bcb70df179ee945f5469934e9778c821f933
Wendell <wendell.lopes.nascimento@gmail.com>
2021-09-17 17:50:08 -0300

    add function TotalPage

--- pagination.go
+++ pagination.go
@@ -5,3 +5,16 @@ type PaginationResult struct {
 	Total int64 `json:"total"`
 	Pages int64 `json:"pages"`
 }
+
+// TotalPage ...
+// totalRows: recebe total registros ex.: 687
+// perPage: recebe total de registros por pagina ex.: 100
+func TotalPage(totalRows int64, perPage int64) (pag int64) {
+	totPage := totalRows / perPage
+
+	rest := totalRows % perPage
+	if rest > 0 {
+		totPage += 1
+	}
+	return totPage
+}
