b1a12228ea9b4d5e074b05db9110cdd4cfd22484
Joacir S. Ramos Junior <contato@joacirjunior.com.br>
2022-02-08 15:32:48 -0300

    Inclusão do X-Current-Identity no Validate do InternalTransactionalToken.

--- transactional_token.go
+++ transactional_token.go
@@ -10,8 +10,10 @@ import (
 )
 
 const (
-	//TransactionTokenHeader ...
+	// TransactionTokenHeader ...
 	TransactionTokenHeader = "X-Transaction-Token"
+	// CurrentIdentityHeader ...
+	CurrentIdentityHeader = "X-Current-Identity"
 )
 
 type TransactionalToken interface {
@@ -54,17 +56,17 @@ func (a *InternalTransactionalToken) Validate() gin.HandlerFunc {
 			return
 		}
 
-		token := c.Request.Header.Get(TransactionTokenHeader)
-
-		if len(token) <= 0 {
-			c.AbortWithStatusJSON(http.StatusForbidden, defaultError)
+		// get token and current identity from header
+		token, currentIdentity, err := getHeaderParameters(c)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusForbidden, err)
 			return
 		}
 
 		payload := struct {
 			Permission string `json:"password,omitempty"`
 		}{
-			Permission: token,
+			Permission: *token,
 		}
 
 		b, err := json.Marshal(payload)
@@ -73,15 +75,20 @@ func (a *InternalTransactionalToken) Validate() gin.HandlerFunc {
 			return
 		}
 
+		// passwords api
 		req, err := http.NewRequest("POST", a.settings.URL, bytes.NewReader(b))
 		if err != nil {
 			c.AbortWithStatusJSON(http.StatusForbidden, defaultError)
 			return
 		}
 
+		// setting headers
 		jwt := c.Request.Header.Get("authorization")
 		req.Header.Set("Content-Type", "application/json")
 		req.Header.Set("Authorization", jwt)
+		if currentIdentity != nil {
+			req.Header.Set("X-Current-Identity", *currentIdentity)
+		}
 
 		client := http.Client{}
 		resp, err := client.Do(req)
@@ -121,4 +128,31 @@ func (a *InternalTransactionalToken) Validate() gin.HandlerFunc {
 
 		c.AbortWithStatusJSON(http.StatusForbidden, defaultError)
 	}
+}
+
+// getHeaderParameters ...
+func getHeaderParameters(c *gin.Context) (*string, *string, error) {
+	defaultError := Error{
+		Code:     http.StatusBadRequest,
+	}
+
+	// get token
+	token := c.Request.Header.Get(TransactionTokenHeader)
+	if len(token) <= 0 {
+		defaultError.Messages = []string{"invalid password"}
+		return nil, nil, &defaultError
+	}
+
+	// get current identity
+	currentIdentity := c.Request.Header.Get(CurrentIdentityHeader)
+	if len(currentIdentity) <= 0 {
+		// TODO Não permitir currentIdentity vazio (apenas após o front alterar para sempre enviar o X-Current-Identity)
+		//// defaultError.Messages = []string{"invalid current identity"}
+		//// return nil, nil, &defaultError
+		return &token, nil, nil
+	} else {
+		currentIdentity = OnlyDigits(currentIdentity)
+	}
+
+	return &token, &currentIdentity, nil
 }
